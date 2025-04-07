c8844c4bffe2fd83e4c837a513a2b708f4be110c
Monah-Haidar <mnhaidar01@gmail.com>
2025-04-07 17:46:41 +0300

    Building Game Cards

--- src/App.tsx
+++ src/App.tsx
@@ -17,10 +17,14 @@ function App() {
                 </GridItem>
 
                 <Show above="lg">
-                    <GridItem area="aside" bg="gold">aside</GridItem>
+                    <GridItem area="aside">
+                        aside
+                    </GridItem>
                 </Show>
 
-                <GridItem area="main"><GameGrid/></GridItem>
+                <GridItem area="main">
+                    <GameGrid/>
+                </GridItem>
 
             </Grid>
         </>
