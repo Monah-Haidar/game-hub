3dcddc838b5396da2a164c31c475847bbf7ff41c
Monah-Haidar <mnhaidar01@gmail.com>
2025-04-08 20:58:10 +0300

    Remove spinner and show a skeleton when genres get displayed

--- src/components/organism/GenreList.tsx
+++ src/components/organism/GenreList.tsx
@@ -1,5 +1,5 @@
 import useGenres from "../../hooks/useGenres.ts";
-import {HStack, List, ListItem, Image, Text, Spinner} from "@chakra-ui/react";
+import {HStack, List, ListItem, Image, Text, Skeleton, SkeletonText, SkeletonCircle} from "@chakra-ui/react";
 import getCroppedImageUrl from "../../services/image-url.ts";
 
 
@@ -7,27 +7,48 @@ const GenreList = () => {
 
     const {data, isLoading, error} = useGenres();
 
+
     if (error) return null;
 
-    if (isLoading) return <Spinner/>;
+    // if (isLoading) return <Spinner/>;
+
+    const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
     return (
-        <List>
+        <>
+            {isLoading && (
+                <List>
+                    {skeletons.map((skeleton) => (
+                        <Skeleton key={skeleton} marginY='5px'>
+                            <HStack>
+                                <SkeletonCircle
+                                    size='10'
+                                />
+                                <SkeletonText/>
+                            </HStack>
+                        </Skeleton>
+                    ))}
+                </List>
+            )}
 
-            {data.map((genre) => (
-                <ListItem key={genre.id} paddingY='5px'>
-                    <HStack>
-                        <Image
-                            boxSize='32px'
-                            borderRadius={8}
-                            src={getCroppedImageUrl(genre.image_background)}
-                        />
-                        <Text fontSize='lg'> {genre.name}</Text>
-                    </HStack>
+            <List>
+                {data.map((genre) => (
+                    <ListItem key={genre.id} paddingY='5px'>
+                        <HStack>
+                            <Image
+                                boxSize='32px'
+                                borderRadius={8}
+                                src={getCroppedImageUrl(genre.image_background)}
+                            />
+                            <Text fontSize='lg'> {genre.name}</Text>
+                        </HStack>
+                    </ListItem>
+                ))}
+            </List>
 
-                </ListItem>
-            ))}
-        </List>
+        </>
+
+
     );
 };
 
