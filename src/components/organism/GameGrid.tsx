1501a002ea1813d83ab0f100091880ebfb129dc8
Monah-Haidar <mnhaidar01@gmail.com>
2025-04-09 22:49:53 +0300

    Refactor GameGrid

--- src/components/organism/GameGrid.tsx
+++ src/components/organism/GameGrid.tsx
@@ -16,24 +16,21 @@ const GameGrid = ({gameQuery}: Props) => {
 
     const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
 
+    if (error) return <Text>{error}</Text>;
+
     return (
-        <>
-            {error && (<Text>{error}</Text>)}
+        <SimpleGrid columns={{
+            sm: 1, md: 2, lg: 3, xl: 4
+        }} spacing={6} padding='10px'>
 
-            <SimpleGrid columns={{
-                sm: 1, md: 2, lg: 3, xl: 4
-            }} spacing={6} padding='10px'>
+            {isLoading && skeletons.map(skeleton => <GameCardContainer key={skeleton}><GameCardSkeleton
+            /></GameCardContainer>)}
 
-                {isLoading && skeletons.map(skeleton => <GameCardContainer key={skeleton}><GameCardSkeleton
-                /></GameCardContainer>)}
+            {data.map((game) => (
+                <GameCardContainer key={game.id}><GameCard game={game}/> </GameCardContainer>
+            ))}
 
-                {data.map((game) => (
-                    <GameCardContainer key={game.id}><GameCard game={game}/> </GameCardContainer>
-                ))}
-
-            </SimpleGrid>
-        </>
-
+        </SimpleGrid>
     );
 };
 
