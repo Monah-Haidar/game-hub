2b36f715e3cdba23b034bf0e2ec5591bec0e1dc8
Monah-Haidar <mnhaidar01@gmail.com>
2025-04-24 14:46:50 +0300

    Refactor: Simplify Game Query

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,8 @@ import GameHeading from "./components/atoms/GameHeading.tsx";
 
 
 export interface GameQuery {
-    genre: Genre | null;
-    platform: Platform | null;
+    genreId?: number;
+    platformId?: number;
     sortOrder: string;
     searchText: string;
 }
@@ -40,8 +40,8 @@ function App() {
 
                 <Show above="lg">
                     <GridItem area="aside" paddingX={5}>
-                        <GenreList selectedGenre={gameQuery.genre}
-                                   onSelectGenre={(genre) => setGameQuery({...gameQuery, genre})}/>
+                        <GenreList selectedGenreId={gameQuery.genreId}
+                                   onSelectGenre={(genre) => setGameQuery({...gameQuery, genreId: genre.id})}/>
                     </GridItem>
                 </Show>
 
@@ -50,8 +50,8 @@ function App() {
                         <GameHeading gameQuery={gameQuery}/>
 
                         <HStack spacing={5} marginBottom={5}>
-                            <PlatformSelector selectedPlatform={gameQuery.platform}
-                                              onSelectPlatform={(platform) => setGameQuery({...gameQuery, platform})}/>
+                            <PlatformSelector selectedPlatformId={gameQuery.platformId}
+                                              onSelectPlatform={(platform) => setGameQuery({...gameQuery, platformId: platform.id})}/>
                             <SortSelector sortOrder={gameQuery.sortOrder}
                                           onSelectSortOrder={(sortOrder) => setGameQuery({...gameQuery, sortOrder})}/>
                         </HStack>
